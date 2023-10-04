24ac4a91e877c35cc511144d50c16aa7afd2982f
campbells <tranbaolam26902@gmail.com>
2023-10-04 21:06:21 +0700

    navigate to home after login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  public handleLogin(): void {
+    this.router.navigate(['/tabs/home']);
+  }
 }
